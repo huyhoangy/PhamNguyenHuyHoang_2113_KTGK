ebb9b21fba05bbf531bb1ffd73c97a9cb1b9213d
huyhoangy <huyhoangwf@gmail.com>
2025-10-18 09:09:52 +0700

    cau4

--- app/src/main/java/com/example/kiemtragiuaky/KqActivity.java
+++ app/src/main/java/com/example/kiemtragiuaky/KqActivity.java
@@ -1,4 +1,60 @@
 package com.example.kiemtragiuaky;
 
-public class KqActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class KqActivity extends AppCompatActivity {
+    TextView tvTrangthai,tvChiso,tvMess;
+    Button btnTinhlai;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_kq);
+        tvTrangthai=findViewById(R.id.tvTrangthai);
+        tvChiso=findViewById(R.id.tvChiso);
+        tvMess=findViewById(R.id.tvMess);
+        btnTinhlai=findViewById(R.id.btnTinhlai);
+        Intent intent =getIntent();
+        float caoo=intent.getFloatExtra("caoo",0);
+        float nangg=intent.getFloatExtra("nangg",0);
+        float caoinm=caoo/100;
+        float bmi=nangg/(caoinm*caoinm);
+        tvChiso.setText(String.format(("%.1f"),bmi));
+        String status,mess;
+        if (bmi<18.5){
+            status="Trang thai: Thiếu cân";
+            mess="Bạn nên ăn thêm";
+        } else if (bmi>18.5&&bmi<24.9) {
+            status="Trang thai: Khỏe mạnh";
+            mess="Bạn có sức khỏe tốt";
+
+        }
+        else if (bmi>25&&bmi<29.9) {
+            status="Trang thai: Thừa cân";
+            mess="Bạn nên ăn ít lại";
+
+        }
+        else{
+            status="Trang thai: Béo phì";
+            mess="Bạn phải giảm cân";
+        }
+        tvTrangthai.setText(status);
+        tvMess.setText(mess);
+        btnTinhlai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
+    }
+
 }
--- app/src/main/java/com/example/kiemtragiuaky/MainActivity.java
+++ app/src/main/java/com/example/kiemtragiuaky/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.kiemtragiuaky;
 
+import static android.app.ProgressDialog.show;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +16,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etCao,etNang;
+    Button btnTinh;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,32 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        etCao=findViewById(R.id.ettCao);
+        etNang=findViewById(R.id.ettNang);
+        btnTinh=findViewById(R.id.btnTinh);
+        btnTinh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String cao=etCao.getText().toString();
+                String nang=etNang.getText().toString();
+                if(!cao.isEmpty()&&!nang.isEmpty())
+                {
+                    float caoo=Float.parseFloat(cao);
+                    float nangg=Float.parseFloat(nang);
+                    Intent intent =new Intent(MainActivity.this,KqActivity.class);
+                    intent.putExtra("caoo",caoo);
+                    intent.putExtra("nangg",nangg);
+                    startActivity(intent);
+
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Vui long nhao du", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+
+
     }
 }
